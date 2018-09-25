b1700b2fcf0ecc4eb7a7e0da1b29afcde51fd7e8
Christoph-T <43521474+Christoph-T@users.noreply.github.com>
2018-09-25 13:43:50 +0200

    create
    create

---
+++ countup_zahler.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned int countDownCounter = 0;
+    while(countDownCounter <=100) {
+            printf("%d\n" , countDownCounter);
+            countDownCounter = countDownCounter +1;
+    }
+    printf("Liftof\n");
+    
+    
+
+    return 0;
+}
+
