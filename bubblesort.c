9f8c6ef9ffcd513400af0085a8844b0d87cccfa9
Christoph-T <43521474+Christoph-T@users.noreply.github.com>
2018-12-05 09:28:53 +0100

    Create bubblesort.c

---
+++ bubblesort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void erzeugeArray(int *, int);
+void zeigeArrayinConsole(int *, int);
+void dreiecksTausch(int *, int);
+
+int main()
+{
+    int array[] = {1, 2, 3, 4, 5, 6};
+    printf("test %d", array[i]);
+    return 0;
+}
+
+void erzeugeArray(int * array, int anz)
+{
+    int i;
+    array[i] = {1, 2, 3, 4, 5, 6};
+}
+
+void zeigeArrayinConsole(int * array, int anz)
+{
+    return 0;
+}
+
+void dreiecksTausch(int * array, int anz)
+{
+    return 0;
+}
