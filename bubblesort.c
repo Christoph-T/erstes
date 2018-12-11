f1c5c6fd5ada30c0d070c74fe558ea515afce2ac
Christoph-T <43521474+Christoph-T@users.noreply.github.com>
2018-12-11 14:48:09 +0100

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -3,26 +3,51 @@
 void erzeugeArray(int *, int);
 void zeigeArrayinConsole(int *, int);
 void dreiecksTausch(int *, int);
+void bubblesort(int *, int);
 
 int main()
 {
-    int array[] = {1, 2, 3, 4, 5, 6};
-    printf("test %d", array[i]);
-    return 0;
+    int anz = 10;
+    int z[anz];
+  
+    erzeugeArray(z, anz);
+
+    printf("\n Array mit %d zufaelligen Zahlen original: \n", anz);
+    zeigeArrayinConsole(z, anz);
+    // bubblesort(z, anz);
+    
+    printf("\n Array mit %d zufaelligen Zahlen sortiert: \n", anz);
+    zeigeArrayinConsole(z, anz);
 }
 
 void erzeugeArray(int * array, int anz)
 {
-    int i;
-    array[i] = {1, 2, 3, 4, 5, 6};
+    srand(5);
+    for(int i = 0; i < anz; i++) {
+        
+        array[i] = rand()%100;
+        
+    }
+   // array[] = {1, 2, 3, 4, 5, 6};
 }
 
 void zeigeArrayinConsole(int * array, int anz)
 {
+    //printf("%s", )
+    for(int i = 0; i < anz; i++) {
+        printf("%d ", array[i]);
+    }
+    
     return 0;
 }
 
 void dreiecksTausch(int * array, int anz)
+{
+    
+    return 0;
+}
+
+void bubblesort(int * array, int anz)
 {
     return 0;
 }
