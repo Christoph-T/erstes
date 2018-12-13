082b5ca790d8dc983260369bc45ae530d375a305
Christoph-T <43521474+Christoph-T@users.noreply.github.com>
2018-12-13 12:29:22 +0100

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -3,22 +3,24 @@
 void erzeugeArray(int *, int);
 void zeigeArrayinConsole(int *, int);
 void dreiecksTausch(int *, int *);
-void bubblesort(int *, int);
+int bubblesort(int *, int);
 
 int main()
 {
-
     int anz = 10;
     int z[anz];
+    int anzY = 0;
+    
     erzeugeArray(z, anz);
     printf("\nArray mit %d zufaelligen Zahlen original: \n", anz);
     zeigeArrayinConsole(z, anz);
-    bubblesort(z, anz);
+    anzY = bubblesort(z, anz);
     printf("\nArray mit %d zufaelligen Zahlen sortiert: \n", anz);
     zeigeArrayinConsole(z, anz);
     printf("\nMaximalzahl der nötigen Vergleiche wäre (%d-1)*(%d-1) = %d", anz, anz, (anz-1)*(anz-1));
-    printf("\nVerbesserte Version (pro Durchlauf 1 Vergleich weniger) wäre (%d + %d + %d...+ 2 + 1) = %d", anz-1, anz-2, anz-3, (anz/2)*(1+anz)); 
-  
+    printf("\nVerbesserte Version (pro Durchlauf 1 Vergleich weniger) wäre (%d + %d + %d...+ 2 + 1) = %d\n", anz-1, anz-2, anz-3, (anz/2)*(1+anz));
+    //anzahl der Vergleiche
+    printf("Tatsächliche benötigte Vergleiche (Ausstieg mit break wenn fertig sortiert) = %d\n", anzY);
 }
 
 void erzeugeArray(int * array, int anz)
@@ -27,6 +29,7 @@ void erzeugeArray(int * array, int anz)
     for(int i = 0; i < anz; i++) {
         array[i] = rand()%100;
     }
+    
 }
 
 void zeigeArrayinConsole(int * array, int anz)
@@ -34,7 +37,7 @@ void zeigeArrayinConsole(int * array, int anz)
     for(int i = 0; i < anz; i++) {
         printf("%d ", array[i]);
     }
-   return 0;
+        return 0;
 }
 
 void dreiecksTausch(int *x, int *y)
@@ -44,13 +47,17 @@ void dreiecksTausch(int *x, int *y)
     *y = temp;
 }
 
-void bubblesort(int * array, int anz)
+int bubblesort(int * array, int anz)
 {
+    int min = 0;
+    int getauscht;
     for(int i = 0; i < anz-1; i++) {
+        getauscht = 0;
         for(int j = 0; j < anz-i-1; j++) {
+            min++;
             if(array[j] > array [j+1])
                 dreiecksTausch(&array[j], &array[j+1]);
         }
     }
-    return 0;
+    return min;
 }
