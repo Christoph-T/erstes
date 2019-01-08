c36c328ec6ef8d928e87b7822792007c31a6e19b
Christoph-T <43521474+Christoph-T@users.noreply.github.com>
2019-01-08 14:51:57 +0100

    Update quicksort_f_tut.c

--- quicksort_f_tut.c
+++ quicksort_f_tut.c
@@ -71,3 +71,61 @@ int main()
     printArray(arr, n); 
     return 0; 
 } 
+
+
+////////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+
+void erzeugeArray();
+void zeigeArrayinConsole();
+void dreieckstausch();
+int teile(int *, int, int);
+void quickSort(int *, int, int);
+
+int main()
+{
+    return 0;
+}
+
+void erzeugeArray(int *x, )
+
+int teile(int *x, int links, int rechts) {
+    
+    int i=links; int j=rechts-1; int pivot=x[rechts];
+    int temp;
+    
+    do {
+        while(x[i]<pivot && i<rechts-1) {
+            i++;
+        }
+        while(x[j]>=pivot && j>links) {
+            j--;
+        }
+        if(i<j) {
+            temp=x[i];
+            x[i]=x[j];
+            x[j]=temp;
+            
+        }
+    } while(i<j);
+    if(x[i]>pivot) {
+        x[rechts]=x[i];
+        x[i] =pivot;
+    }
+
+    return i;
+}
+
+void quickSort(int *x, int links, int rechts) {
+    int teiler = 0;
+    if(links < rechts) {
+        
+    teiler = teile(x, links, rechts);
+    quickSort(x, links, rechts);
+    quickSort(x, teiler = 1, rechts);
+    
+    }
+}
+
